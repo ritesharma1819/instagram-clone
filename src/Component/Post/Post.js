6d483223213f44bb45a7562bd29d4b7e4d6d1c32
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-03 21:08:06 +0530

    added styling of post

--- src/Component/Post/Post.js
+++ src/Component/Post/Post.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import './Post.css';
 
 function Post() {
   return (
-    <div>
-        <h3>Username</h3>
-        <img 
+    <div className='post'>
+        <h4 className='post__username'>Username</h4>
+        <img className='post__img'
             src='https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg' 
             alt='post' 
         />
-        <p><strong>Caption:</strong> Hey there i am making instagram clone!</p>
+        <p className='post__caption'><strong>Caption:  </strong>Hey there i am making instagram clone!</p>
     </div>
   )
 }
