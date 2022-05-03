6f1ea7071d68975d824a38941c900aace6a19a5c
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-03 20:47:22 +0530

    added file of post

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Component/Header/Header';
+import Post from './Component/Post/Post';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Post />
     </div>
   );
 }
---
+++ src/Component/Post/Post.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+        <h3>Username</h3>
+        <img 
+            src='https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg' 
+            alt='post' 
+        />
+        <p><strong>Caption:</strong> Hey there i am making instagram clone!</p>
+    </div>
+  )
+}
+
+export default Post
