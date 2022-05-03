e88e729915b10f3b1757d4e660a1bafd359ec007
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-03 21:14:17 +0530

    added a thin boader line after header

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginRight: '15px' , backgroundColor: 'white'}}>
+    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between' , backgroundColor: 'white' , borderBottom: '1px solid lightGray'}}>
         <img 
             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLF3H-LTyPl-Pql1UaLM6-KIprIhlt42JivwgsCVkgTX8b5sOJeRZ6SITTLqtFA2lBAhI&usqp=CAU'
             alt='logo'
