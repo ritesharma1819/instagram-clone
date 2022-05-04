39e130276e1932626dda20ff06ed6cc25c453fc4
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-04 19:16:53 +0530

    connected firebase with our code

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyDKpUZS1Oc9gaj5glZDkn85VWUXxmFoCfo",
+    authDomain: "instagram-clone-eb68c.firebaseapp.com",
+    projectId: "instagram-clone-eb68c",
+    storageBucket: "instagram-clone-eb68c.appspot.com",
+    messagingSenderId: "769064192327",
+    appId: "1:769064192327:web:c115b2c1c89a39c6006462",
+    measurementId: "G-LYPW305Q1W"
+  });
+
+  const db= firebase.firestore();
+  const auth=firebase.auth();
+  const storage= firebase.storage();
+
+  export{db, auth, storage};
