d471fb9054c342e7f719c88ad542d603b65cebee
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-04 22:05:06 +0530

    give to our firebase data inside useEffect

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React,{useState ,useEffect} from 'react';
 import './App.css';
 import Header from './Component/Header/Header';
 import Post from './Component/Post/Post';
+import {db} from './firebase'
 
 function App() {
   const [posts, setPosts]=useState([])
+
+useEffect(()=>{
+      db.collection("posts").onSnapshot(snapshot=>{
+        setPosts(snapshot.docs.map(doc=>doc.data()))
+      })
+})
 
   return (
     <div className="App">
