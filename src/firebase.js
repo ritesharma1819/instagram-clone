71541ea0f24bd835b9a417e218201cba6931f3e7
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-05 19:20:57 +0530

    minnor change in firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
+import 'firebase/compat/storage';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -16,6 +17,6 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
   const db= firebase.firestore()
   const auth= firebase.auth()
-
+  const storage=firebase.storage()
   
-  export {db, auth};
+  export {db, auth ,storage};
