2deada34448c645266f5dac14073441a11c7dd5a
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-06 00:12:12 +0530

    done styling of file input and submit button

--- src/Component/ImageUpload/ImageUpload.js
+++ src/Component/ImageUpload/ImageUpload.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import firebase from 'firebase/compat/app';
 import { db, storage } from '../../firebase';
-import { width } from '@mui/system';
 
 function ImageUpload() {
    const [caption, setCaption]= useState('')
@@ -56,8 +55,8 @@ function ImageUpload() {
                 <h3>Upload File</h3>
                 <progress value={progress} max='100'/>
                 <input style={{padding: '5px', width: '30%', borderRadius: '5px' , fontSize: 'larger'}} type='text' placeholder='write caption here...' value={caption} onChange={(e)=>setCaption(e.target.value)}/>
-                <input type='file' onChange={handleChange}/>
-                <button type='submit' onClick={handleUpload}>Upload</button>
+                <input style={{ padding: '5px', marginLeft: '30px',width: '30%', cursor: 'pointer' }} type='file' onChange={handleChange}/>
+                <button style={{marginBottom: '10px' , backgroundColor: 'purple' , color: 'white' , cursor: 'pointer'}} type='submit' onClick={handleUpload}>Upload</button>
             </div>
         </form>
     </div>
