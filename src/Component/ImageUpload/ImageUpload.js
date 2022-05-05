112bd5a4bf0243b617bf87f92ea7ba1e3722cd2c
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-05 23:30:31 +0530

    added uploading post feature

--- src/Component/ImageUpload/ImageUpload.js
+++ src/Component/ImageUpload/ImageUpload.js
@@ -1,23 +1,57 @@
 import React, { useState } from 'react';
-import { storage } from '../../firebase';
+import firebase from 'firebase/compat/app';
+import { db, storage } from '../../firebase';
 
 function ImageUpload() {
    const [caption, setCaption]= useState('')
    const [image, setImage]= useState(null)
+   const [progress, setProgress]=useState(0)
 
    const handleChange=(e)=>{
-       if(e.target.file[0]){
+       if(e.target.files[0]){
             setImage(e.target.files[0])
        }
    }
    
-   const handleUpload=()=>{
-       const uploadTask= storage.ref(`image/${image.name}`).put(image);
+   const handleUpload=(event)=>{
+       event.preventDefault();
+       const uploadTask= storage.ref(`images/${image.name}`).put(image);
+
+       uploadTask.on(
+           "state_changed",
+           (snapshot)=>{
+               const progress= Math.round(
+                   (snapshot.bytesTransferred/snapshot.totalBytes)*100
+               );
+               setProgress(progress);
+           },
+           (error)=>{
+               console.log(error);
+               alert(error.message);
+           },
+           ()=>{
+               storage
+               .ref("images")
+               .child(image.name)
+               .getDownloadURL()
+               .then(url=>{
+                   db.collection("posts").add({
+                       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                       caption: caption,
+                       imageUrl: url
+                   });
+                   setProgress(0);
+                   setCaption('');
+                   setImage(null);
+               });
+           }
+       )
    }
 
   return (
     <div>
         <form>
+            <progress value={progress} max='100'/>
             <input type='text' placeholder='write caption here...' value={caption} onChange={(e)=>setCaption(e.target.value)}/>
             <input type='file' onChange={handleChange}/>
             <button type='submit' onClick={handleUpload}>Upload</button>
