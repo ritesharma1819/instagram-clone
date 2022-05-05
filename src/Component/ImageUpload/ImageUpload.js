301d101a7f7ac068fba1295010a701e14a9e89f0
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-05 20:51:19 +0530

    give state for caption and file

--- src/Component/ImageUpload/ImageUpload.js
+++ src/Component/ImageUpload/ImageUpload.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { storage } from '../../firebase';
 
 function ImageUpload() {
+   const [caption, setCaption]= useState('')
+   const [image, setImage]= useState(null)
+
+   const handleChange=(e)=>{
+       if(e.target.file[0]){
+            setImage(e.target.files[0])
+       }
+   }
+   
+   const handleUpload=()=>{
+       const uploadTask= storage.ref(`image/${image.name}`).put(image);
+   }
+
   return (
-    <div>ImageUpload</div>
+    <div>
+        <form>
+            <input type='text' placeholder='write caption here...' value={caption} onChange={(e)=>setCaption(e.target.value)}/>
+            <input type='file' onChange={handleChange}/>
+            <button type='submit' onClick={handleUpload}>Upload</button>
+        </form>
+    </div>
   )
 }
 
