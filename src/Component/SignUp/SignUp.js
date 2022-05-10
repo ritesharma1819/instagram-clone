8167064a7766a3ded2012fd293d2617dd0612712
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-10 14:21:35 +0530

    added input fields in sigUp component

--- src/Component/SignUp/SignUp.js
+++ src/Component/SignUp/SignUp.js
@@ -2,7 +2,33 @@ import React from 'react'
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div>
+        <form>
+             <div style={{display: 'flex' , 
+                flexDirection: 'column', 
+                alignItems: 'center' ,
+                marginTop: '100px',
+                border: '1px solid lightgray' , 
+                width: '300px',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                gap: '5px'
+             }}>
+                <img 
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLF3H-LTyPl-Pql1UaLM6-KIprIhlt42JivwgsCVkgTX8b5sOJeRZ6SITTLqtFA2lBAhI&usqp=CAU'
+                    alt='logo'
+                    width='200px'
+                    style={{marginTop: '50px', marginBottom: '30px'}}
+                />                                                  
+                <input style={{ padding: '5px', width: '200px'}} type='text' placeholder='Username' />
+                <input style={{ padding: '5px', width: '200px'}} type='email' placeholder='Email' />
+                <input style={{padding: '5px', width: '200px', marginBottom: '5px'}} type='password' placeholder='password' />
+                <button 
+                    style={{ padding: '5px', width: '215px', backgroundColor: '#5851DB' , color:'white'}}
+                >Sign Up</button>
+             </div>
+         </form>
+    </div>
   )
 }
 
