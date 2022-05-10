03782a44118088395fd7f6d2df4138f9d7ff283c
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-10 13:44:48 +0530

    do styling of sign in component

--- src/Component/SignIn/SignIn.js
+++ src/Component/SignIn/SignIn.js
@@ -4,14 +4,26 @@
    return (
      <div>
          <form>
-            <img 
-                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLF3H-LTyPl-Pql1UaLM6-KIprIhlt42JivwgsCVkgTX8b5sOJeRZ6SITTLqtFA2lBAhI&usqp=CAU'
-                alt='logo'
-                width='200px'
-            />
-             <input type='email' placeholder='Email' />
-             <input type='password' placeholder='password' />
-             <button>SignIn</button>
+             <div style={{display: 'flex' , 
+                flexDirection: 'column', 
+                alignItems: 'center' , 
+                border: '1px solid lightgray' , 
+                width: '300px',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                gap: '5px'
+             }}>
+                <img 
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLF3H-LTyPl-Pql1UaLM6-KIprIhlt42JivwgsCVkgTX8b5sOJeRZ6SITTLqtFA2lBAhI&usqp=CAU'
+                    alt='logo'
+                    width='200px'
+                    style={{marginTop: '50px', marginBottom: '30px'}}
+                />                                                  
+                <input style={{ padding: '5px', width: '200px'}} type='email' placeholder='Email' />
+                <input style={{padding: '5px', width: '200px', marginBottom: '5px'}} type='password' placeholder='password' />
+                <button style={{ padding: '5px', width: '215px', backgroundColor: '#5851DB' , color:'white'}}>Sign In</button>
+                <button style={{ padding: '5px', width: '215px', backgroundColor: '#5851DB' , color:'white', marginBottom: '50px'}}>Sign Up</button>
+             </div>
          </form>
      </div>
    )
