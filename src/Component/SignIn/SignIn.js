652e01cd29546d255f6314c293ada4673375a8b2
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-10 13:18:23 +0530

    added input field in signIn component

--- src/Component/SignIn/SignIn.js
+++ src/Component/SignIn/SignIn.js
@@ -2,7 +2,18 @@
  
  function SignIn() {
    return (
-     <div>SignIn</div>
+     <div>
+         <form>
+            <img 
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLF3H-LTyPl-Pql1UaLM6-KIprIhlt42JivwgsCVkgTX8b5sOJeRZ6SITTLqtFA2lBAhI&usqp=CAU'
+                alt='logo'
+                width='200px'
+            />
+             <input type='email' placeholder='Email' />
+             <input type='password' placeholder='password' />
+             <button>SignIn</button>
+         </form>
+     </div>
    )
  }
  
